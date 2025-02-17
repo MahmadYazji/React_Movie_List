5e8e27489c3f541eea6d3a9a198c0a5547d391ad
MohammedYazji <mahmadyazji@gmail.com>
2025-02-17 11:19:13 +0200

    UploadThree

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  // Replace 'your-repo-name' with the actual repository name
+  base: "/your-repo-name/",
   plugins: [react()],
-})
+});
